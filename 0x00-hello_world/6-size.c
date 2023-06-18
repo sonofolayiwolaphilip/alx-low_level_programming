14526e7729bf175bef2a31ad850ea0154fd20055
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-06-18 13:48:54 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
