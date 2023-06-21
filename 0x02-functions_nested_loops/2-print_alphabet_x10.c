22554f181c72b0dc14c21bf75cf0846b2675293f
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-06-21 14:59:34 -0700

    two integer add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - Adds two integers
+* @a: The number 0ne integer to add 
+* @b: The number two interger to add
+*
+* Return: total sum of the two integers
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
