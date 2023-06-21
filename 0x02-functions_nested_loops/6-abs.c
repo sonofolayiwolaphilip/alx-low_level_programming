38a965a90389d6fa152cd314c4d4a62e90777dc0
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-06-21 15:19:24 -0700

    absolute value compute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+ * @n: The integer for which the absolute value is computed
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+		if (n < 0)
+			return (-n);
+		else
+			return (n);
+		_putchar('\n');
+}
