3f4c11adcc63d4e79083d7db70c916e4d4d7f41d
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-18 06:06:17 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - main structure name
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ */
 struct dog
 {
 	char *name;
