5ca45c2a6bcb5988716b815f738ed7e9015e8a5d
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-18 07:47:45 -0700

    initializing variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,6 +13,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d->name = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
 
 	strcpy(d->name, name);
 	d->age = age;
