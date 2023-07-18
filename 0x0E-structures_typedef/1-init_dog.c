eec59727ebd1cdcc56d7c55d3f19c09d35bcd583
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-18 08:53:34 -0700

    initials

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,11 +13,14 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
+	if (*d == NULL)
+	{
+		*d = malloc(sizeof(struct dog));
+	}
 
-	d->age = age;
+	(*d)->name = name;
 
-	d->owner = owner;
+	(*d)->age = age;
+
+	(*d)->owner = owner;
 }
