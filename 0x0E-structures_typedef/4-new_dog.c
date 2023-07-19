819a58e10e940fbe8a4f97fa0c896b545f52867d
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-18 18:18:19 -0700

    pointers

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if 
+
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,8 @@ void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
 
+dog_t *new_dog(char *name, float age, char *owner);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
