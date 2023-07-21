9cbc44e8a998c0ffe10fbdb3f1c1fadf2b260aaf
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-21 05:54:58 -0700

    print functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/* print_all - print anything function
+ * @char: the string data type
+ */
+
+void print_all(const char * const format, ...)
+{
+
+}
