afbd2f6563905b0ae9429c6aba054a2075f73971
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-21 10:58:28 +0100

    variadics

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,17 +1,28 @@
-sum_them_all(const unsigned int n, ...)
-{
-	int k, l, result;
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - variadic function for summing arguments
+ * @n: last fixed argument before variable arguments
+ *
+ * Return: 0 if n is 0
+ */
+int sum_them_all(const unsigned int n, ...){
+	va_list mynumb;
+
+	int result = 0;
+	unsigned int i;
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list mynumb;
+
 	va_start(mynumb, n);
 
-	k = va_arg(mynumb, int);
-	l = va_arg(mynumb, int);
+	for(i = 0; i < n; i++)
+		result += va_arg(mynumb, int);
 
-	result = k + l;
 	va_end(mynumb);
 	return (result);
 }
