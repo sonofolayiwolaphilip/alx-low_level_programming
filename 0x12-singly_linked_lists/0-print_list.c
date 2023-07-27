1e491e22cb1aa1c8d7f5a0bc0370952b6765992b
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-07-27 13:38:52 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,17 +13,14 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (count);
-	}
 	while (h != NULL)
 	{
-		printf("%s ", h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n",(unsigned int) h->len, h->str);
 		h = h->next;
 		count++;
 	}
-	printf("\n");
 	return (count);
 }
