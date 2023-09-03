2a4885886c1f903feba500c7d06faac3bd1e748b
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-03 12:53:11 -0700

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,14 +3,15 @@
  * main - program that prints all arguments it receives
  * @argc: counter
  * @argv: values passed
+ * Return: 0 on success
  */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	if (argc > 0)
 	{
-		for(i=0; i < argc; i++)
+		for (i = 0; i < argc; i++)
 		{
 			printf("%s\n", argv[i]);
 		}
