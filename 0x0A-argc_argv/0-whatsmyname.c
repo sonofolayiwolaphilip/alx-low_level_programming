d3cf61f856dc6391efd2e0ce6880b7e23fd2e029
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-03 11:09:26 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,15 @@
 #include "main.h"
 /**
  * main - to print self name
+ * @argc: counter
+ * @argv: place holder
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	}
 	return (0);
 }
