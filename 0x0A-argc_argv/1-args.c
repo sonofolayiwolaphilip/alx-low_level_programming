03ef30fa8966fa06e8f376578428183e2f4785fc
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-03 11:39:23 -0700

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - to print number of arguments
- * argc: counter
- * argv: variables
+ * @argc: counter
+ * @argv: variables
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
