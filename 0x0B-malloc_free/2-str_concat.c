fb876f256463994c59e3027353b88240c9e3d71d
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-05 13:43:14 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s1 = "";
 	}
 	ptr = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 	if (ptr == NULL)
