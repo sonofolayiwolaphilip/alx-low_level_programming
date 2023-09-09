179bbdb0a828dceecc67a3ff19fc6c9cd7a5e944
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-09 00:49:14 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,18 +11,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int s2_len, i;
+	char *ptr;
+	size_t len1, len2, result_len;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	char *ptr = (char *)malloc(strlen(s1) + n + 1);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (len1 < n)
+	{
+		result_len = len1 + len2;
+	}
+	else
+	{
+		result_len = len1 + n;
+	}
+	ptr = (char *)malloc(result_len + 1);
 
 	if (ptr == NULL)
 	{
@@ -31,18 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		strcpy(ptr, s1);
-		if (n >= strlen(s2))
-		{
-			strcat(ptr, s2);
-		}
-		else
-		{
-			s2_len = strlen(s2);
-			for (i = 0; i < n; i++)
-			{
-				strcat(ptr, &s2[i]);
-			}
-		}
+		strncat(ptr, s2, n);
 		return (ptr);
 	}
 }
