b7d8faa37fda8abc3c6516abf81d05282e8a7899
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-11 13:34:09 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - name of structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+#endif
