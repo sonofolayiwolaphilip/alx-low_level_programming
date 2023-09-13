ce7d6f04d20017bf876e7dccce1dca61e5a60d8f
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-12 21:00:00 -0700

    update

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that print the name of the file it is compiled from
+ * Return - To return 0 on success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
