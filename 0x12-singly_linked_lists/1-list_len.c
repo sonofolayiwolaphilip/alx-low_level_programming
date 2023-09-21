068e8939416025038086e03665986dae04c63596
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-09-21 15:13:43 -0700

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * lists_len - function that returns the number of elements in a linked
-* @h:associating argument
-* Return: appropriate values 
-*/
+ * list_len - function that returns the number of elements in a linked
+ * @h:associating argument
+ * Return: appropriate values
+ */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
