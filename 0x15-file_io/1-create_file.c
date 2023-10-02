27189a8248ef924f9d167ae368e8f2d33fc93ae2
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-10-03 00:41:17 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,23 +7,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filled, win, lenght = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (lenght = 0; text_content[lenght];)
-			lenght++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	filled = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0060);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	win = write(filled, text_content, lenght);
+	w = write(fd, text_content, len);
 
-	if (filled == -1 || win == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	close(filled);
+	close(fd);
 	return (1);
 }
