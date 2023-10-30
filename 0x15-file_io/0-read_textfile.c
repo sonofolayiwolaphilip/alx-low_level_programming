40837a01ffa723d6d7d3c1c662922a7ba5b13e82
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-10-30 20:52:46 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buffing = malloc(sizeof(char) * letters);
 	tyre = read(feed, buffing, letters);
-	w = write(STDOUT_FILENO, buffing, tyre);
+	width = write(STDOUT_FILENO, buffing, tyre);
 	free(buffing);
 	close(feed);
 	return (width);
